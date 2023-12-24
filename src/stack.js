ba64d9c1860e6d5210a89aafcc5c6c9672ac837e
Tanya Holikava <holikavadev@gmail.com>
2023-12-24 20:19:50 +0300

    fix: change arr on obj in constructor

--- src/stack.js
+++ src/stack.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Stack {
   constructor() {
-    this.dataStore = [];
+    this.dataStore = {};
     this.size = 0;
   }
 
